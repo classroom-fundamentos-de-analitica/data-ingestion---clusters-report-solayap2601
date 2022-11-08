6d7cbaa13151d5cd65cad279f1f72795cd902d9a
solayap2601 <solayap@unal.edu.co>
2022-11-08 01:40:35 -0500

    :+1:

--- pregunta.py
+++ pregunta.py
@@ -10,7 +10,7 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
 
@@ -18,4 +18,30 @@ def ingest_data():
     # Inserte su código aquí
     #
 
+    filename = 'clusters_report.txt'
+    with open(filename, mode = 'r') as clusters_report:
+        report = clusters_report.readlines()
+
+    clusters = []
+    cluster = [0, 0, 0, '']
+
+    for fila in report[4:]:
+        if re.match('^ +[0-9]+ +', fila):
+            lista = fila.split()
+            cluster[0] = int(lista[0])
+            cluster[1] = int(lista[1])
+            cluster[2] = float(lista[2].replace(',','.'))
+            cluster[3] = ' '.join(lista[4:])
+
+        elif re.match('^ +[a-z]', fila):
+            palabras = fila.split()
+            palabras = ' '.join(palabras)
+            cluster[3] += ' ' + palabras
+
+        elif re.match('^\n', fila) or re.match('^ +$', fila):
+            cluster[3] = cluster[3].replace('.', '')
+            clusters.append(cluster)
+            cluster = [0, 0, 0, '']
+
+    df = pd.DataFrame (clusters, columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
     return df
